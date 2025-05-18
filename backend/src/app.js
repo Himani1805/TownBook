0fd56a72c4f35b7d1d6b495380db7887e8ed8520
Himani Sharma <hinusharma18@gmail.com>
2025-05-18 12:58:24 +0530

    .

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,7 +30,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(cors({
-    origin: ['https://hs-townbook.vercel.app', 'http://localhost:5173', '*'],
+    origin: ['https://town-book-two.vercel.app', 'http://localhost:5173', '*'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
